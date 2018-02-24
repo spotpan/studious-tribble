6ab7deda189e1babe285e42e0bd7ae1b3ef70797
spotpan <yb960@nyu.edu>
2018-02-24 13:06:11 -0500

    ok

--- head.c
+++ head.c
@@ -2,92 +2,88 @@
 #include "stat.h"
 #include "user.h"
 
-char buf[3];
+char buf[512];
 
-void
-head(int fd, char *name, int x)
+void head(int fd, int x)
 {
   int i, n;
-  int l, w, c;
+  int l;
 
-  l = w = c = 0;
+  l = 0;
 
   while((n = read(fd, buf, sizeof(buf))) > 0 && l<x)
   {
-	
+
     for(i=0;i<=n && l<x;i++)
     {
       if(buf[i]!='\n')
       {
       	printf(1,"%c",buf[i]);
-      }		
+      }
       else
       {
       	printf(1,"\n");
       	l++;
       }
-      
-      
-      
     }
-
   }
+
   if(n < 0)
   {
     printf(1, "head: read error\n");
-   exit();
+    exit();
    }
 }
 
-int
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
   int fd, i;
 
   if(argc <= 1)
   {
-    head(0, "", 10);
+    head(0, 10);
     exit();
   }
 
-else if(argc==2)
-{
-  for(i = 1; i < argc; i++)
+  else if(argc==2)
+
+  {
+    for(i=1; i < argc; i++)
+      {
+        if((fd = open(argv[i], 0)) < 0){
+          printf(1, "head: cannot open %s\n", argv[i]);
+          exit();
+        }
+        head(fd,10);
+        close(fd);
+      }
+  }
+
+
+  else if(argc==3)
+  {
+    char c[512];
+    strcpy(c,argv[1]);
+    char *num_str = c;
+    num_str=num_str+1;
+    int x= atoi(num_str);
+
+
+    for(i=2; i < argc; i++)
     {
       if((fd = open(argv[i], 0)) < 0){
         printf(1, "head: cannot open %s\n", argv[i]);
         exit();
       }
-      head(fd, argv[i],10);
+      head(fd,x);
       close(fd);
     }
-}
-
-  
-else if(argc==3)
-{
-char c[512];
-strcpy(c,argv[1]);
-char *num_str = c;
-num_str=num_str+1;
-int x= atoi(num_str);
-
-
-  for(i = 2; i < argc; i++)
-  {
-    if((fd = open(argv[i], 0)) < 0){
-      printf(1, "head: cannot open %s\n", argv[i]);
-      exit();
-    }
-    head(fd, argv[i],x);
-    close(fd);
   }
-}
 
-else
-{
-  printf(1,"head: syntax error");
+  else
+  {
+    printf(1,"head: syntax error");
+  }
+    exit();
+  
 }
-  exit();
-}
-
