8a3e7a47448e8c5075d0fcf4f4c32dbadb29f4c1
spotpan <yb960@nyu.edu>
2018-02-24 19:03:09 -0500

    ok

--- head.c
+++ head.c
@@ -6,84 +6,44 @@ char buf[512];
 
 void head(int fd, int x)
 {
-  int i, n;
-  int l;
-
-  l = 0;
+  int i, n, l=0;
 
   while((n = read(fd, buf, sizeof(buf))) > 0 && l<x)
   {
-
     for(i=0;i<=n && l<x;i++)
     {
-      if(buf[i]!='\n')
-      {
-      	printf(1,"%c",buf[i]);
-      }
-      else
-      {
-      	printf(1,"\n");
-      	l++;
-      }
+      if(buf[i]!='\n'){printf(1,"%c",buf[i]);}
+      else{printf(1,"\n");l++;}
     }
   }
 
-  if(n < 0)
-  {
-    printf(1, "head: read error\n");
-    exit();
-   }
+  if(n < 0){printf(1, "head: read error\n");exit();}
 }
 
 int main(int argc, char *argv[])
 {
-  int fd, i;
+  int fd, i, x=10;
+  char c[512];
+  char *num_str = c;
 
-  if(argc <= 1)
-  {
-    head(0, 10);
-    exit();
-  }
-
-  else if(argc==2)
-
-  {
-    for(i=1; i < argc; i++)
-      {
-        if((fd = open(argv[i], 0)) < 0){
-          printf(1, "head: cannot open %s\n", argv[i]);
-          exit();
-        }
-        head(fd,10);
-        close(fd);
-      }
-  }
-
-
-  else if(argc==3)
-  {
-    char c[512];
+ if(argc <= 1){head(0, 10);exit();}
+ i=1;
+ if(argv[1][0]=='-'){
+    
     strcpy(c,argv[1]);
-    char *num_str = c;
     num_str=num_str+1;
-    int x= atoi(num_str);
+    x= atoi(num_str);
+    if(argc <= 2){head(0,x);exit();}
+    i=2;
+ }
 
-
-    for(i=2; i < argc; i++)
+ for(; i < argc; i++)
     {
-      if((fd = open(argv[i], 0)) < 0){
-        printf(1, "head: cannot open %s\n", argv[i]);
-        exit();
-      }
-      head(fd,x);
-      close(fd);
+       if((fd = open(argv[i], 0)) < 0){
+          printf(1, "head: cannot open %s\n", argv[i]);
+          exit();}
+       head(fd,x);
     }
-  }
-
-  else
-  {
-    printf(1,"head: syntax error");
-  }
-    exit();
-  
+ close(fd);
+ exit();
 }
