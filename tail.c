5e708ac81b6cb92d90a9b50e85aeb738fda89119
spotpan <yb960@nyu.edu>
2018-02-27 16:28:07 -0500

    ok

--- tail.c
+++ tail.c
@@ -1,13 +1,14 @@
+
 #include "types.h"
 #include "stat.h"
 #include "user.h"
 
-char buf[1024];
+char buf[512];
 
 void
-tail(char* filepath)
+tail(int fd, char* filepath, int x)
 {
-  int fd, n, m, count;
+  int n, m, count;
   char *p, *q;
   memset(buf, 0, sizeof(char)*1024);
   if((fd = open(filepath, 0)) < 0){
@@ -37,7 +38,7 @@ tail(char* filepath)
     printf(1, "tail: cannot open %s\n", filepath);
     exit();
   }
-  if(count <= 10)
+  if(count <= x)
   {
     while((n = read(fd, buf, sizeof(buf))) > 0)
       write(1, buf, n);
@@ -53,7 +54,7 @@ tail(char* filepath)
     p = buf;
     while((q = strchr(p, '\n')) != 0){     
       count--;
-      if(count < 10)
+      if(count < x)
       {
         *q = '\n';
         write(1, p, q+1 - p);
@@ -71,14 +72,29 @@ tail(char* filepath)
 
 }
 
-int
-main(int argc, char *argv[])
-{ 
-  if(argc < 2){
-    printf(2, "usage: tail filename ...\n");
-    exit();
-  }
-  
-  tail(argv[1]);
-  exit();
+
+
+int main(int argc, char *argv[])
+{
+  int fd, i, x=10;
+  char c[512];
+  char *num_str = c;
+
+ if(argc <= 1){tail(0,"",10);exit();} i=1;
+
+ if(argv[1][0]=='-'){ 
+    strcpy(c,argv[1]);
+    num_str=num_str+1;
+    x= atoi(num_str);
+    if(argc <= 2){tail(0,"",x);exit();}i=2;
+ }
+
+ for(; i < argc; i++){
+       if((fd = open(argv[i], 0)) < 0){
+       printf(1, "head: cannot open %s\n", argv[i]);
+       exit();}
+     tail(fd,argv[i],x);
+    }
+ close(fd);
+ exit();
 }
